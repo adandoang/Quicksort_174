786faf4ea8a8f4d4fa429e3426f88bc8339dc66b
adandoang <akhdanjzif@gmail.com>
2023-03-31 11:02:44 +0700

    menamakan void input

--- Quicksort_174/Quicksort_174.cpp
+++ Quicksort_174/Quicksort_174.cpp
@@ -5,6 +5,17 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n masukan panjang array adalah 20" << endl;
 
 	}
-}
+	
+
